2108bd504680c01de443a11fe99428646234fad3
rh1n0 <apollocoin@hushmail.com>
2014-08-16 06:27:44 -0600

    genesis block

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2");
+uint256 hashGenesisBlock("0x9e640581ee0f3fff36f89b8c2421bff41bfd168846409f202656a2e63da7b570");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // MP3Coin: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2789,7 +2789,7 @@ bool InitBlockIndex() {
         block.nVersion = 1;
         block.nTime    = 1408189908;
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 0;
+        block.nNonce   = 90579;
 
         if (fTestNet)
         {
@@ -2802,7 +2802,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("0x77ca6f14f19061c54bedaffc7ec65b2cfc5bee256c77a9501fc92b8576c8fee1"));
         block.print();
         assert(hash == hashGenesisBlock);
 
